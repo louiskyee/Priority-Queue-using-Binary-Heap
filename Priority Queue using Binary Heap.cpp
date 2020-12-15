9f535ba67950330254062a00de9c64f2a0054c11
louiskyee <45658859+louiskyee@users.noreply.github.com>
2020-12-15 15:06:31 +0800

    Update Priority Queue using Binary Heap.cpp

--- Priority Queue using Binary Heap.cpp
+++ Priority Queue using Binary Heap.cpp
@@ -3,99 +3,136 @@
 #include <vector>
 #include <queue>
 using namespace std;
-struct Type {
-	char letter;
-	int order;
+char reletter = -878;
+int reorder = -878;
+class Type {
+public:
+	char letter = reletter;
+	int order = reorder;
+	friend bool operator==(Type a, Type b) {
+		if (a.letter == b.letter && a.order == b.order) return true;
+		return false;
+	}
 };
-struct Node {
-	char data;
+struct Node{
+	Type data;
 	Node* left;
 	Node* right;
+	Node* parent;
+	int N = 0;
 };
 class Tree {
-private:
-	Node* newNode(char _data) {
+private:	
+	Node* newNode() {
 		Node* temp = new Node;
-		temp->data = _data;
-		temp->left = temp->right = NULL;
+		temp->data.letter = reletter;
+		temp->data.order = reorder;
+		temp->left = temp->right = temp->parent = NULL;
 		return temp;
 	}
-	Node* _insert(vector<Type> arr, Node* node, int index) {
-		if (index < arr.size()) {
-			Node* temp = newNode(arr[index].letter);
-			node = temp;
-			
-			node->left = _insert(arr, node->left, index * 2 + 1);
-			node->right = _insert(arr, node->right, index * 2 + 2);
+	void swap(Type& a, Type& b) {
+		Type temp = a;
+		a = b;
+		b = temp;
+	}
+	void inspect(Node* _node) {
+		if (_node->parent) {
+			if (_node->data.order < _node->parent->data.order) {
+				swap(_node->data, _node->parent->data);
+			}
+			inspect(_node->parent);
 		}
-		return node;
+	}
+	void _insert(queue<Node*>& _qu, Type _data) {
+		_qu.front()->data = _data;
+		_qu.front()->parent->N++;
+		_qu.front()->left = newNode(); _qu.front()->left->parent = _qu.front();
+		_qu.front()->right = newNode(); _qu.front()->right->parent = _qu.front();
+		_qu.push(_qu.front()->left);
+		_qu.push(_qu.front()->right);
+		inspect(_qu.front());
+		_qu.pop();
+	}
+	Node* _delete_empty(Node* _node) {		
+		if (_node->left) { _node->left = _delete_empty(_node->left); }
+		if (_node->right) {	_node->right = _delete_empty(_node->right); }
+		if (_node->data.order == reorder) {
+			delete[] _node;
+			_node = NULL;
+		}
+		return _node;
+	}
+	Node* arrange(Node* _node) {
+		if (_node->left && _node->right) {
+			if (_node->left->data.order < _node->right->data.order) {
+				swap(_node->left->data, _node->data);
+				_node->left = arrange(_node->left);
+			}
+			else {
+				swap(_node->right->data, _node->data);
+				_node->right = arrange(_node->right);
+			}
+		}
+		else if (_node->left) {
+			swap(_node->left->data, _node->data);
+			_node->left = arrange(_node->left);
+		}
+		else if (_node->right) {
+			swap(_node->right->data, _node->data);
+			_node->right = arrange(_node->right);
+		}
+		else {
+			delete[] _node;
+			_node = NULL;
+		}
+		return _node;
 	}
 public:
-	Node* root;
-	void insert(vector<Type> arr) {
-		root = _insert(arr, root, 0);
+	Node* root = NULL;
+	queue<Node*> qu;
+	void insert(Type _data) {
+		if (root == NULL) {
+			root = newNode();
+			root->data = _data;
+			root->left = newNode(); root->left->parent = root;
+			root->right = newNode(); root->right->parent = root;
+			qu.push(root->left);
+			qu.push(root->right);
+		}
+		else {
+			_insert(qu, _data);
+		}
+	}
+	void delete_empty() {
+		if (root) {
+			root = _delete_empty(root);
+		}
+	}
+	void print() {
+		while (root != NULL) {
+			cout << root->data.letter;
+			root = arrange(root);
+		}		
 	}
 };
-void print(Tree tree) {
-	queue<Node*> qu;
-	qu.push(tree.root);
-	while (!qu.empty()) {
-		cout << qu.front()->data;
-		if (qu.front()->left) qu.push(qu.front()->left);
-		if (qu.front()->right) qu.push(qu.front()->right);
-		qu.pop();
-	}
-}
-void swap(Type& a, Type& b) {
-	Type temp = a;
-	a = b;
-	b = temp;
-}
-int partition(vector<Type>& arr, int left, int right) {
-	int loc = left;
-	while (true) { 
-		while (arr[right].order >= arr[loc].order && loc != right) {	//arr[right] >= arr[loc]
-			right--;
-		}
-		if (loc == right) return loc;
-		else {	//loc > right
-			swap(arr[loc], arr[right]);
-			loc = right;
-		}
-		while (arr[loc].order >= arr[left].order && loc != left) {	//arr[loc] >= arr[left]
-			left++;
-		}
-		if (loc == left) return loc;
-		else{	//left > loc
-			swap(arr[loc], arr[left]);
-			loc = left;
-		}
-	}
-}
-void quickSort(vector<Type>& arr, int begin, int end) {
-	if (begin < end) {
-		int loc = partition(arr, begin, end);
-		quickSort(arr, begin, loc - 1);
-		quickSort(arr, loc + 1, end);
-	}
-}
 int main(void) {
-	vector<Type> arr;
 	Tree tree;
 	string str;
 	getline(cin, str);
 	Type temp;
 	int order = 0;
 	str += ';';
-	for (int i = 0; str[i] != '\0'; ++i) {		
+	for (int i = 0; str[i] != '\0'; ++i) {
 		if (str[i] == ' ') { temp.letter = str[i - 1]; order = 0; }
-		else if (str[i] == ';') { temp.order = order; arr.push_back(temp);}
+		else if (str[i] == ';') {
+			temp.order = order;
+			tree.insert(temp); 
+		}
 		else {
 			order *= 10;
 			order += str[i] - '0';
 		}
 	}
-	quickSort(arr, 0, arr.size() - 1);
-	tree.insert(arr);
-	print(tree);
+	tree.delete_empty();
+	tree.print();
 }
